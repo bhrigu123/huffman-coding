1d198dcddf40374b4569fa523ac81586804a7c31
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-01-01 17:04:00 +0000

    removed bin files

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 pip3 install pytest
 pip3 install autopep8
+
+cp gitignore .gitignore
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 alias autopep8="/home/runner/.local/bin/autopep8  --in-place --aggressive --aggressive --indent-size 2"
 
-
+cp gitignore .gitignore
 cd tests
 /home/runner/.local/bin/pytest
